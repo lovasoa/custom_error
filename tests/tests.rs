1ad5be3cfd97a0b447f3f827be3f46fb0ce4ee84
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-03-08 22:36:01 +0100

    Improve tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,10 +1,25 @@
 #[macro_use]
 extern crate custom_error;
 
-use std::{fs::File, io, io::Read, num::ParseIntError, result::Result};
+use std::{fs::File, io, io::Read, io::ErrorKind, num::ParseIntError, result::Result};
+
+custom_error!{
+    OpenFileError
+        NotFound{filename: String} = "Tried to open '{filename}', but it doesn't exist",
+        Other                      = "An unknown I/O error occured.",
+}
+
+/// Opens a file with a verbose error on failure
+fn open_file_verbose(filename: &str) -> Result<File, OpenFileError> {
+    File::open(filename).map_err(|e| match e.kind() {
+            ErrorKind::NotFound => OpenFileError::NotFound{filename: filename.to_string()},
+            _ => OpenFileError::Other
+    })
+}
 
 custom_error! {FileParseError
-    Io{source: io::Error}         = "unable to read from the file",
+    Open{source: OpenFileError}   = @{ source }, // Chained custom error
+    Io{source: io::Error}         = "I/O error",
     Format{source: ParseIntError} = "the file does not contain a valid integer",
     TooLarge{value:u8}            = "the number in the file ({value}) is too large"
 }
@@ -12,7 +27,7 @@ custom_error! {FileParseError
 fn parse_hex_file(filename: &str) -> Result<u8, FileParseError> {
     let mut contents = String::new();
     // The '?' notation can convert from generic errors to our custom error type
-    File::open(filename)?.read_to_string(&mut contents)?;
+    open_file_verbose(filename)?.read_to_string(&mut contents)?;
     let value = u8::from_str_radix(&contents, 16)?;
     if value > 42 {
         Err(FileParseError::TooLarge { value })
@@ -24,5 +39,8 @@ fn parse_hex_file(filename: &str) -> Result<u8, FileParseError> {
 #[test]
 fn main() {
     let parse_result = parse_hex_file("/i'm not a file/");
-    assert_eq!("unable to read from the file", parse_result.unwrap_err().to_string());
+    assert_eq!(
+        "Tried to open '/i'm not a file/', but it doesn't exist",
+        parse_result.unwrap_err().to_string()
+    );
 }
